a6e0baee2f6066025bd4c955fb65aef4cda3735e
Denuwan Yasodhana <denuwanyasodhana123@gmail.com>
2024-09-18 14:38:02 +0530

    Update todo.dart

--- lib/model/todo.dart
+++ lib/model/todo.dart
@@ -11,12 +11,12 @@ class ToDo {
 
   static List<ToDo> todoList() {
     return [
-      ToDo(id: '01', todoText: 'Morning Exercise', isDone: true),
-      ToDo(id: '02', todoText: 'Buy Groceries', isDone: true),
-      ToDo(id: '03', todoText: 'Check mails'),
-      ToDo(id: '04', todoText: 'Team metting'),
-      ToDo(id: '05', todoText: 'Work on mobile app for 2 houts'),
-      ToDo(id: '06', todoText: 'Dinner with Jenny'),
+      ToDo(id: '01', todoText: 'Plan a weekend trip', isDone: true),
+      ToDo(id: '02', todoText: 'Buy tickets for the cricket match', isDone: true),
+      ToDo(id: '03', todoText: 'Attend online webinar'),
+      ToDo(id: '04', todoText: 'Clean the house'),
+      ToDo(id: '05', todoText: 'Complete assignment for university'),
+      ToDo(id: '06', todoText: 'Reply to emails regarding project'),
     ];
   }
 }
